25e40d9db53fe50f559aff16075fc0fb381041a5
Zehvogel <l@tm-r.de>
2017-07-25 22:51:08 +0200

    added argparse and fixed output

--- apery.py
+++ apery.py
@@ -1,12 +1,25 @@
 #/usr/bin/python3
 
+import argparse
+import sys
 import secrets
 import math
 
 
-MAX_RND = 1000
-N = 10000000
+#MAX_RND = 1000
+#N = 10000000
 
+
+def get_arguments():
+    parser = argparse.ArgumentParser(description='''A small program to
+                                     calculate Apéry's constant using 
+                                     (pseudo) random numbers.''')
+    parser.add_argument('-N', default=1000, help='''The amount of random
+                        Numbers used. Defaults to 1000.''', type=int)
+    parser.add_argument('-M', default=1000, help='''The maximum size any
+                        random number can take. Defaults to 1000''', type=int)
+    args = parser.parse_args()
+    return args
 
 def gcd(a, b, c):
     d = math.gcd(a,b)
@@ -19,25 +32,28 @@ def tuple_gcd(t):
     a, b, c = t
     return gcd(a, b, c)
 
-def get_three_rnds():
-    a = secrets.randbelow(MAX_RND)
-    b = secrets.randbelow(MAX_RND)
-    c = secrets.randbelow(MAX_RND)
+def get_three_rnds(max_rnd):
+    a = secrets.randbelow(max_rnd)
+    b = secrets.randbelow(max_rnd)
+    c = secrets.randbelow(max_rnd)
     return (a, b, c)
 
-def get_rnd_list(length):
+def get_rnd_list(length, max_rnd):
     rnd_list = []
     for i in range(length):
-        rnd_list.append(get_three_rnds())
+        rnd_list.append(get_three_rnds(max_rnd))
     return rnd_list
 
 def main():
+    args = get_arguments()
     coprimes = 0
-    rnd_list = get_rnd_list(N)
+    N = args.N
+    M = args.M
+    rnd_list = get_rnd_list(N, M)
     for i in rnd_list:
         if tuple_gcd(i) == 1:
             coprimes += 1
-    print("N: {}\n", N)
-    print("Number of coprimes: {}\n", coprimes)
-    print("Approximation of Apery's constant: {}\n", N/coprimes)
+    print("N: {}".format(N))
+    print("Number of coprimes: {}".format(coprimes))
+    print("Approximation of Apery's constant: {}".format(N/coprimes))
 main()
